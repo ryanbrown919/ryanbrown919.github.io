f75635a9e3e3541b4ba130d5fa5daf0b1fa6a029
Ryan Brown <103395728+ryanbrown919@users.noreply.github.com>
2025-02-17 12:04:47 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.context_processor
+def inject_now():
+    return {'current_year': datetime.now().year}
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
